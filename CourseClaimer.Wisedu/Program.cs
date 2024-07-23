b4b35b5f3215a347462c03bccc82e2873628e77b
MadLongTom <36219016+MadLongTom@users.noreply.github.com>
2024-07-23 15:19:56 +0800

    chore(HttpClient): UsePooledConnection

--- CourseClaimer.Wisedu/Program.cs
+++ CourseClaimer.Wisedu/Program.cs
@@ -80,6 +80,8 @@ builder.Services.AddHttpClient("JWXK", client =>
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0");
     client.DefaultRequestHeaders.Connection.Add("keep-alive");
 }).AddHttpMessageHandler<MapForwarderHandler>()
+    .UseSocketsHttpHandler()
+    .SetHandlerLifetime(TimeSpan.FromHours(14))
   .AddStandardResilienceHandler(options =>
 {
     options.Retry.MaxRetryAttempts = 3;
